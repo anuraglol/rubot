0064769bc69b746f66a605bf395aba463c174284
Anurag <77309809+kr-anurag@users.noreply.github.com>
2021-10-14 14:27:33 +0000

    added movie command less go

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js')
-const keepAlive = require('./server')
 const axios = require('axios').default;
 const { MessageEmbed } = require('discord.js');
 
@@ -23,59 +22,6 @@ client.on('ready', () => {
 })
 
 client.on("messageCreate", async message => {
-  if (message.content === '$help') {
-  
-  message.channel.send("commands: \n `$help`: *help me out!* \n `$ping`: *replies with pong!* \n `$gm`: *replies with gm!* \n `$hey`: *replies with hey, but in a funny way* \n `$wassup`: *pings user* \n `$user`: *replies with the user(s) name* \n `$pic`: *replies with the avatar of the user* \n `$cat`: *replies with a random cat picture* \n `$dog`: *replies with a random doggo pic* ")
-
-  }
-
-  else if (message.content === '$ping') {
-    message.channel.send("pong!")
-  }
-
-  else if (message.content === '$gm') {
-    message.channel.send('gm!')
-  }
-
-  else if (message.content === '$hey') {
-    message.channel.send('hey sussy baka')
-  }
-
-  else if (message.content === '$wassup') {
-    message.channel.send(`wassup, ${message.author.toString()}`)
-  }
-
-  else if (message.content === '$user') {
-    message.channel.send(message.author.username)
-  }
-
-  else if (message.content === '$pic') {
-    message.channel.send(message.author.avatarURL())
-  }
-
-  else if (message.content === '$cat') {
-    message.channel.send('meow')
-    axios.get('https://aws.random.cat/meow')
-      .then(function(res) {
-        message.channel.send(res.data.file)
-      })
-  }
-
-  else if (message.content === '$dog') {
-    axios.get('https://random.dog/woof.json')
-      .then(function(res) {
-        message.channel.send(res.data.url)
-      })
-  }
-
-  else if(message.content === '$chuck-joke'){
-    axios.get('https://geek-jokes.sameerkumar.website/api?format=json')
-    .then(function(res){
-      message.channel.send(res.data.joke)
-    })
-  }
-
-
   let prefix = '$'
   
   const args = message.content.slice(prefix.length).trim().split(/ +/);
@@ -91,7 +37,8 @@ client.on("messageCreate", async message => {
     else (
       axios.get(`http://www.omdbapi.com/?apikey=5c401357&t=${args.join("+")}`)
         .then(function(res) {
-          
+          console.log(res.data)
+
           const movieEmbed = new MessageEmbed()
             .setColor('#0099ff')
             .setTitle(res.data.Title)
@@ -117,5 +64,4 @@ client.on("messageCreate", async message => {
   }
 })
 
-keepAlive()
 client.login(process.env['TOKEN'])
